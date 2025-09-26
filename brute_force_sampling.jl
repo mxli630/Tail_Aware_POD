98dde5cf1050ea78c8eb7c549e3d77553816263e
Mingxin Li <130235612+mxli630@users.noreply.github.com>
2025-09-26 17:11:49 -0400

    Update brute_force_sampling.jl

--- brute_force_sampling.jl
+++ brute_force_sampling.jl
@@ -4,17 +4,18 @@ using FileIO, JLD2
 numruns = 10000
 paramlen = 3*num_eigenvectors+1
 all_x = Array{Float64}(undef, paramlen, numruns)
+all_h = Array{Float64}(undef, nfreenode, numruns)
 all_f = Float64[]
 
 for i = 1:numruns
   x = randn(paramlen)
   all_x[:,i] = x
-  push!(all_f, f(x))
+  hfree = solveforhfree(x2logKs(x))
+  all_h[:,i] = hfree
+  push!(all_f, full(hfree)[critical_point_node]*9.807*997*1e-6)
 end
 
-fsort = sort(all_f, rev = true);
-sortind = sortperm(all_f, rev = true);
-all_x_sorted = all_x[:,sortind]
 
 FileIO.save("all_f.jld2","all_f",all_f)
 FileIO.save("all_x.jld2","all_x",all_x)
+FileIO.save("all_h.jld2","all_h",all_h)
