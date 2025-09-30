6b87f4e707d94995ac00d2a46d032b7e9b0e74bd
Georg Stadler <stadler@cims.nyu.edu>
2025-09-30 17:48:30 -0400

    minor notation/cleanup

--- generate_tail_sample.jl
+++ generate_tail_sample.jl
@@ -6,11 +6,9 @@
 #   and evaluate the Quantity of Interest (QoI).
 #
 # Notes:
-#   - The rate function is `penalty_func_lambda(x,mu,lambda)`
-#   - The rate function depends on the constants `mu`, `lambda`,
-#     and `z`.
-#   - `mu` and `lambda` are defined for numerical purpose, so that
-#     the optimization program can successfully converge.
+#   - The ALM function combining the rate function and the constraint $f(x)>=z$
+#     is `penalty_func_lambda(x,mu,lambda)`
+#   - `mu` and `lambda` are updated .
 #   - `z` is the value for where the tail of QoI starts. In our 
 #     example, the tail is chosen as the top 1% of QoI.
 #   - The rate function depends on constants associated with the
@@ -25,12 +23,17 @@
 
 include("supplement_functions.jl")
 
+
+z = 0.0011
+
 penalty_func_lambda(x, mu, lambda, z) = 0.05*(transpose(x)*x) - lambda*(f(x) - z) + (mu/2)*((f(x) - z)^2);
 penalty_grad_lambda(x, mu, lambda, z) = 0.1.*x + (mu*(f(x)-z)-lambda).*grad_f(x);
 
-lambda = 1024; mu = 1e8; z = 0.0011
+lambda = 1024; mu = 1e8;
 x = randn(3*num_eigenvectors+1)
 x = randn(3*num_eigenvectors+1)
+
+# ALM method
 res = SciPy.optimize.fmin_bfgs(penalty_func_lambda,x,fprime = penalty_grad_lambda, args = (mu,lambda,z), maxiter = 40, gtol = 1e-5, full_output = 1, retall = true);
 x_res = res[1];
 
